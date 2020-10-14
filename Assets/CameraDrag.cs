04facd694adb25ff9258dae6197b0e589f32b5b7
ryfprivate <ryfprivate@outlook.com>
2020-10-15 02:24:29 +1000

    Added basic camera drag

---
+++ Assets/CameraDrag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    public float camBoundary = 21f;
+    public float dragSpeed;
+    private Vector3 dragOrigin;
+
+    void Start()
+    {
+        dragSpeed = 0.2f;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+        float newX = transform.position.x - move.x;
+        if (newX >= 0 && newX <= camBoundary)
+        {
+            transform.Translate(-move, Space.World);
+        }
+    }
+}
