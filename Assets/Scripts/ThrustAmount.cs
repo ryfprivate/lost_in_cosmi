73b2a1c3740554b90925be5a1cbfa111e05244dc
ryfprivate <ryfprivate@outlook.com>
2020-10-14 23:53:35 +1000

    Renamed ThrustLevel to ThrustAmount

--- Assets/Scripts/ThrustAmount.cs
+++ Assets/Scripts/ThrustAmount.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ThrustLevel : MonoBehaviour
+public class ThrustAmount : MonoBehaviour
 {
     public Text thrustText;
     public PlayerLaunch pl;
