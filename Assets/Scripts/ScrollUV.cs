4692da584c17bac34294eb8f5abe22da494d356a
ryfprivate <ryfprivate@outlook.com>
2020-10-17 04:26:18 +1000

    Added parallax background

---
+++ Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+    public float parallax = 2f;
+    private MeshRenderer mr;
+    private Material mat;
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        mat = mr.material;
+        mat.mainTexture.wrapMode = TextureWrapMode.Repeat;
+    }
+    void Update()
+    {
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x = transform.position.x / transform.localScale.x / parallax;
+        offset.y = transform.position.x / transform.localScale.y / parallax;
+
+        mat.mainTextureOffset = offset;
+    }
+}
