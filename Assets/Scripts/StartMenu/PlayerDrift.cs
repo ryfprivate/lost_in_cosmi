6c5a9746fbc28e7797da7907f43ff9bda1691203
ryfprivate <ryfprivate@outlook.com>
2020-10-17 13:23:16 +1000

    Added music

---
+++ Assets/Scripts/StartMenu/PlayerDrift.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDrift : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private float speed = 10f;
+    void Start()
+    {
+        rb.AddForce(transform.up * speed, ForceMode2D.Impulse);
+    }
+}
