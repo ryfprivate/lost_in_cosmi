05e550b23820a47d435e8922acb03ce46cd61af9
ryfprivate <ryfprivate@outlook.com>
2020-10-17 23:33:35 +1000

    Added zoom functionality for mobile

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -25,18 +25,22 @@ public class CameraMovement : MonoBehaviour
             initialMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
-        // if (Input.touchCount == 2)
-        // {
-        //     Touch touchZero = Input.GetTouch(0);
-        //     Touch touchOne = Input.GetTouch(1);
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
 
-        //     Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-        // }
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
 
+            float difference = currentMagnitude - prevMagnitude;
 
-        // During the touch
-        if (Input.GetMouseButton(0))
+            Zoom(difference * 0.01f);
+        }
+        else if (Input.GetMouseButton(0))
         {
             Vector3 direction = initialMousePosition - Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Camera.main.transform.position += direction;
