5e4a859b837cc130fba6eab242b0daa916e250b6
ryfprivate <ryfprivate@outlook.com>
2020-10-22 18:19:40 +1000

    Added text for charge power on cannon

--- Assets/Scripts/TextThrustController.cs
+++ Assets/Scripts/TextThrustController.cs
@@ -6,15 +6,40 @@ using UnityEngine.UI;
 public class TextThrustController : MonoBehaviour
 {
     public Text thrustText;
+    private Vector3 startPos;
+    private Vector3 target;
+    private float t;
+    private float animTime;
+    private bool charging;
+
     void Start()
     {
         GameEvents.current.onTriggerCharge += UpdateText;
+        GameEvents.current.onTriggerCharge += ShiftText;
+
+        charging = false;
+        t = 0;
+        animTime = 0.66f;
     }
 
     void UpdateText(float thrust)
     {
         int percentage = (int)(thrust / CannonLaunchController.maxThrust * 100);
         thrustText.text = percentage.ToString() + "%";
+    }
+
+    void ShiftText(float thrust)
+    {
+        if (!charging)
+        {
+            startPos = transform.position;
+            target = startPos - 0.21f * transform.up;
+            charging = true;
+        }
+
+        t += Time.deltaTime / animTime;
+
+        transform.position = Vector3.Lerp(startPos, target, t);
     }
 
     void onDestroy()
