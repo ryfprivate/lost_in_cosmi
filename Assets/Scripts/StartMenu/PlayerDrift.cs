eec9ecc425badcc10f2510d3011294872e79ca19
ryfprivate <ryfprivate@outlook.com>
2020-10-22 21:26:55 +1000

    Disabled death in start menu

--- Assets/Scripts/StartMenu/PlayerDrift.cs
+++ Assets/Scripts/StartMenu/PlayerDrift.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerDrift : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator death;
     private float speed = 10f;
+
     void Start()
     {
+        death.speed = 0;
         rb.AddForce(transform.up * speed, ForceMode2D.Impulse);
     }
 }
