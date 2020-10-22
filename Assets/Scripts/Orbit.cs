19910731b33c18ec8f4a0897bb727dea1cf9e839
ryfprivate <ryfprivate@outlook.com>
2020-10-23 01:56:43 +1000

    Added orbit script

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform parent;
+    private float orbitSpeed;
+
+    void Start()
+    {
+        orbitSpeed = 10.0f;
+    }
+
+    void Update()
+    {
+        transform.position = RotatePointAroundPivot(transform.position,
+                           parent.position,
+                           Quaternion.Euler(0, 0, orbitSpeed * Time.deltaTime));
+    }
+
+    Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle)
+    {
+        return angle * (point - pivot) + pivot;
+    }
+}
